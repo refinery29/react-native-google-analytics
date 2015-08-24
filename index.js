c0bdc93b982ea2fb94ad44554ba62c3d25ce3687
Ilana Sufrin <ilanasufrin@gmail.com>
2015-08-24 14:39:33 -0400

    maybe adds custom dimension

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
     PageView: require('./lib/hits/PageView'),
     ScreenView: require('./lib/hits/ScreenView'),
     Social: require('./lib/hits/Social'),
-    Timing: require('./lib/hits/Timing')
+    Timing: require('./lib/hits/Timing'),
+    CustomDimension: require('./lib/hits/CustomDimension')
   },
   Analytics: require('./lib/Analytics'),
   Experiment: require('./lib/Experiment'),
